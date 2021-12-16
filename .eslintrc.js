98ef49d1fe3b75ac7067e5133733294343dfbee4
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 00:33:33 +0100

    Add types and fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -16,10 +19,15 @@ module.exports = {
     },
     ecmaVersion: 13,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   plugins: [
     'react',
     '@typescript-eslint',
+  ],
+  ignorePatterns: [
+    '.eslintrc.js'
   ],
   rules: {
     'no-console': 'off',
---
+++ src/@types/BlocksType.d.ts
@@ -0,0 +1,9 @@
+type BlocksType = {
+  id: string,
+  defaultX: number,
+  defaultY: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+}
