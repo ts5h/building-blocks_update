c37236fabfcb13111a8f39805cb6459a5025ca35
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 00:35:11 +0100

    Format files

--- src/@types/BlocksType.d.ts
+++ src/@types/BlocksType.d.ts
@@ -1,9 +1,9 @@
 type BlocksType = {
-  id: string,
-  defaultX: number,
-  defaultY: number,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
+  id: string
+  defaultX: number
+  defaultY: number
+  x: number
+  y: number
+  width: number
+  height: number
 }
--- src/data/BlocksData.ts
+++ src/data/BlocksData.ts
@@ -1,13 +1,13 @@
 const blocks: BlocksType[] = [
   {
-    id: "block_1",
+    id: 'block_1',
     defaultX: 0,
     defaultY: 0,
     x: 0,
     y: 0,
     width: 20,
-    height: 20
-  }
+    height: 20,
+  },
 ]
 
 export default blocks
