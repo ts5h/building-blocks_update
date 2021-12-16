99fbc15b2aad3e42acc83694ac6b0bc4b72a2cc2
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-16 20:33:27 +0100

    Fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,23 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'no-console': 'off',
+    semi: 0,
+    'react/jsx-one-expression-per-line': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 };
