ee459c16ab2d49dd80556ed62c9cc9cf841fdb87
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 23:56:03 +0100

    Fix elements dragging problems

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -5,7 +5,7 @@ import Styles from '../scss/components/Playground.module.scss'
 
 // Playground
 const Playground = () => {
-  const position = useMousePosition()
+  const movement = useMousePosition()
   const [isDrag, setIsDrag] = useState(false)
   const [current, setCurrent] = useState<HTMLDivElement | null>(null)
 
@@ -19,34 +19,42 @@ const Playground = () => {
     setCurrent(null)
   }
 
+  window.addEventListener('mouseup', onMouseUpHandler)
+
   useEffect(() => {
-    if (isDrag && current) {
-      const blockPosition = current.getBoundingClientRect()
-      let left = position.x
-      let top = position.y
+    const onMoveHandler = () => {
+      if (isDrag && current) {
+        const blockPosition = current.getBoundingClientRect()
+        let left = movement.x + blockPosition.x + window.scrollX
+        let top = movement.y + blockPosition.y + window.scrollY
 
-      if (left < 0) {
-        left = 0
-      } else if (left > 2000 - blockPosition.width) {
-        left = 2000 - blockPosition.width
+        if (left < 0) {
+          left = 0
+        } else if (left > 2000 - blockPosition.width) {
+          left = 2000 - blockPosition.width
+        }
+
+        if (top < 0) {
+          top = 0
+        } else if (top > 2000 - blockPosition.height) {
+          top = 2000 - blockPosition.height
+        }
+
+        current.style.left = `${left}px`
+        current.style.top = `${top}px`
       }
-
-      if (top < 0) {
-        top = 0
-      } else if (top > 2000 - blockPosition.height) {
-        top = 2000 - blockPosition.height
-      }
-
-      current.style.left = `${left}px`
-      current.style.top = `${top}px`
     }
+    window.addEventListener('mousemove', onMoveHandler)
 
-    window.addEventListener('mouseup', onMouseUpHandler)
-  }, [isDrag, current, position])
+    return () => {
+      window.removeEventListener('mousemove', onMoveHandler)
+    }
+  }, [current, isDrag, movement, movement.x, movement.y])
 
   return (
     <div className={Styles.playground}>
       <Block id="block_1" isDrag={isDrag} setCurrentElement={setCurrentElement} />
+      <Block id="block_2" isDrag={isDrag} setCurrentElement={setCurrentElement} />
     </div>
   )
 }
--- src/components/UseMousePosition.ts
+++ src/components/UseMousePosition.ts
@@ -4,10 +4,10 @@ const useMousePosition = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    const setFromEvent = (e: { clientX: number; clientY: number }) =>
+    const setFromEvent = (e: { movementX: number; movementY: number }) =>
       setPosition({
-        x: e.clientX,
-        y: e.clientY,
+        x: e.movementX,
+        y: e.movementY,
       })
 
     window.addEventListener('mousemove', setFromEvent)
