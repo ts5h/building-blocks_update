a518bccd9fe1ab7a06a5a33105d6e356a1468ce9
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 23:07:37 +0100

    Update functions

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import isMobile from 'ismobilejs'
 import Styles from '../scss/components/Block.module.scss'
 
 // Each block
 type BlockProps = {
-  setCurrent: (arg0: HTMLDivElement) => void
+  id: string
+  isDrag: boolean
+  setCurrentElement: (arg0: boolean, arg1: HTMLDivElement | null) => void
 }
 
 const Block = (props: BlockProps) => {
-  const { setCurrent } = props
+  const { id, isDrag, setCurrentElement } = props
+  const [, idNum] = id.split('_')
+  const [zIndex, setZIndex] = useState(0)
+
+  useEffect(() => {
+    if (isDrag) {
+      setZIndex(200)
+    } else {
+      setZIndex(parseInt(idNum, 10))
+    }
+  }, [idNum, isDrag])
 
   const onMouseDownHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (!isMobile().any) {
-      setCurrent(e.currentTarget)
+      setCurrentElement(true, e.currentTarget)
     }
   }
 
   const onTouchStartHandler = (e: React.TouchEvent<HTMLDivElement>) => {
     if (isMobile().any) {
-      setCurrent(e.currentTarget)
+      setCurrentElement(true, e.currentTarget)
     }
   }
 
@@ -30,8 +42,9 @@ const Block = (props: BlockProps) => {
       onTouchStart={(e) => onTouchStartHandler(e)}
       onDragStart={(e) => e.preventDefault()}
       className={Styles.block}
+      style={{ zIndex }}
     >
-      block_1
+      {id}
     </div>
   )
 }
