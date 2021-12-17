011df5c81356e7c79d9617da4b9d7c8219883424
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 11:20:51 +0100

    Add BlocksData

---
+++ test.ts
@@ -0,0 +1,49 @@
+const blocks: BlocksType[] = [
+  {
+    id: 'block_1',
+    defaultX: 242,
+    defaultY: 286,
+    x: 581,
+    y: 264,
+    width: 34,
+    height: 17,
+  },
+  {
+    id: 'block_2',
+    defaultX: 119,
+    defaultY: 307,
+    x: 326,
+    y: 353,
+    width: 24,
+    height: 34,
+  },
+  {
+    id: 'block_3',
+    defaultX: 36,
+    defaultY: 88,
+    x: 75,
+    y: 146,
+    width: 25,
+    height: 35,
+  },
+  {
+    id: 'block_4',
+    defaultX: 309,
+    defaultY: 315,
+    x: 544,
+    y: 153,
+    width: 13,
+    height: 17,
+  },
+  {
+    id: 'block_5',
+    defaultX: 250,
+    defaultY: 116,
+    x: 561,
+    y: 73,
+    width: 37,
+    height: 37,
+  },
+]
+
+export default blocks
