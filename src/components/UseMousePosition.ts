d4eb5127a4f109572b92bda8b9829174d3542aa7
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 13:44:45 +0100

    Add UseMousePosition component / Update Block to be able to tracking mouse position

---
+++ src/components/UseMousePosition.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+const useMousePosition = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const setFromEvent = (e: { clientX: number; clientY: number }) =>
+      setPosition({
+        x: e.clientX,
+        y: e.clientY,
+      })
+
+    window.addEventListener('mousemove', setFromEvent)
+    return () => {
+      window.removeEventListener('mousemove', setFromEvent)
+    }
+  }, [])
+
+  return position
+}
+
+export default useMousePosition
