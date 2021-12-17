032486d61e6c6e60e4dac307ca8622c858b1a786
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-17 17:52:32 +0100

    Update functions

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -6,14 +6,47 @@ import Styles from '../scss/components/Playground.module.scss'
 // Playground
 const Playground = () => {
   const position = useMousePosition()
-  const [state, setState] = useState(false)
+  const [isDrag, setIsDrag] = useState(false)
+  const [current, setCurrent] = useState<HTMLDivElement | null>(null)
+
+  const onMoveHandler = () => {
+    if (isDrag && current) {
+      const blockPosition = current.getBoundingClientRect()
+      let left = position.x - blockPosition.width / 2
+      let top = position.y - blockPosition.height / 2
+
+      if (left < 0) {
+        left = 0
+      }
+
+      if (top < 0) {
+        top = 0
+      }
+
+      current.style.left = `${left}px`
+      current.style.top = `${top}px`
+    }
+  }
+
+  const setCurrentElement = (div: HTMLDivElement) => {
+    setIsDrag(true)
+    setCurrent(div)
+  }
+
+  const onMouseUpHandler = () => {
+    setIsDrag(false)
+    setCurrent(null)
+  }
+
+  window.addEventListener('mousemove', onMoveHandler)
+  window.addEventListener('mouseup', onMouseUpHandler)
 
   // TODO: Show all blocks
   // TODO: Get coordination and write db
 
   return (
-    <div className={Styles.playground}>
-      <Block position={position} />
+    <div className={`playground ${Styles.playground}`}>
+      <Block setCurrent={setCurrentElement} />
     </div>
   )
 }
