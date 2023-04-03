b005affa738779f47479bff830a61fd0b2be1bfa
at4 <at4@jc4.so-net.ne.jp>
2023-04-03 22:23:50 +0200

    Add test sound files

--- src/store/global/Sound/index.ts
+++ src/store/global/Sound/index.ts
@@ -1,6 +1,12 @@
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export const useSound = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const filePath = "assets/sounds/";
+
+  const audioContext = useRef(new AudioContext());
+  const [audioBuffer, setAudioBuffer] = useState<AudioBuffer >();
+
   // TODO: Play and stop sound logic
   const playLoopSound = useCallback((fileName: string) => {
     console.log("play sound");
