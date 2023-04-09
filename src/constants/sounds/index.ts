0202456ed09970d1549abe00afd2a49850b9e5e0
at4 <at4@jc4.so-net.ne.jp>
2023-04-09 13:54:21 +0200

    Replace sound files

--- src/constants/sounds/index.ts
+++ src/constants/sounds/index.ts
@@ -1,5 +1,12 @@
 export const sounds = [
-  "Hulusi_A2.mp3",
-  "HulusiPlayPhrase_1_C.mp3",
-  "HulusiPlayPhrase_4_B.mp3",
+  "1_Kurdish_Female_words.mp3",
+  "2_Arabic_spoken_words.mp3",
+  "5_Khmer_vocals_M_Part.mp3",
+  "8_Kurdish_Fiddle_loop_1.mp3",
+  "9_Tibetan_male_vox_2.mp3",
+  "18.mp3",
+  "30_male_throat_vox_1.mp3",
+  "64_Chinese_Bamboo_Flute_1.mp3",
+  "Guitar.mp3",
+  "Voice_042_sd.mp3",
 ];
