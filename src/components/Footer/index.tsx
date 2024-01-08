bf5591eff4209cf806919441e7a74324a95d5758
at4 <at4@jc4.so-net.ne.jp>
2024-01-08 22:42:07 +0100

    Add speech bubble

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,24 @@
-import React, { FC, useState } from "react";
-import { MdOutlineHelpOutline } from "react-icons/md";
+import React, { FC, useEffect, useRef, useState } from "react";
+import { MdOutlineHelpOutline, MdOutlineClose } from "react-icons/md";
 import Styles from "../../scss/components/Footer.module.scss";
 
 export const Footer: FC = () => {
   const [isHelpOpen, setIsHelpOpen] = useState(false);
+
+  const navRef = useRef<HTMLDivElement>(null);
+  const helpRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // const nav = navRef.current;
+    // const help = helpRef.current;
+  }, [isHelpOpen]);
 
   const handleClick = () => setIsHelpOpen(!isHelpOpen);
 
   return (
     <footer className={Styles.footer}>
       <p>&copy; 2023 0bjekt/ts5h, James Cleeland</p>
-      <nav className={Styles.nav}>
+      <nav className={Styles.nav} ref={navRef}>
         <button
           type="button"
           className={isHelpOpen ? Styles.on : ""}
@@ -19,6 +27,20 @@ export const Footer: FC = () => {
           <p>Help</p>
           <MdOutlineHelpOutline />
         </button>
+        <div className={Styles.help} ref={helpRef}>
+          <button
+            className={Styles.close}
+            type="button"
+            onClick={handleClick}
+          >
+            <p>Close</p>
+            <MdOutlineClose />
+          </button>
+          <ul>
+            <li>Each shape displayed on the page can be moved by dragging and dropping.</li>
+            <li>Each shape plays a sound when dragging.</li>
+          </ul>
+        </div>
       </nav>
     </footer>
   );
