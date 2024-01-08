6d2d252a568861a7dc57b5d1232c154b3af48cac
at4 <at4@jc4.so-net.ne.jp>
2024-01-08 21:56:31 +0100

    Update the footer layouts

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,24 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { MdOutlineHelpOutline } from "react-icons/md";
 import Styles from "../../scss/components/Footer.module.scss";
 
 export const Footer: FC = () => {
+  const [isHelpOpen, setIsHelpOpen] = useState(false);
+
+  const handleClick = () => setIsHelpOpen(!isHelpOpen);
+
   return (
     <footer className={Styles.footer}>
       <p>&copy; 2023 0bjekt/ts5h, James Cleeland</p>
-      <nav>
-        <MdOutlineHelpOutline className={Styles.button} onClick={() => {}} />
+      <nav className={Styles.nav}>
+        <button
+          type="button"
+          className={isHelpOpen ? Styles.on : ""}
+          onClick={handleClick}
+        >
+          <p>Help</p>
+          <MdOutlineHelpOutline />
+        </button>
       </nav>
     </footer>
   );
