e7e5d2debd3c67ecbc80cbd859537789cb885ef1
at4 <at4@jc4.so-net.ne.jp>
2024-01-08 19:38:58 +0100

    Add a help button

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from "react";
+import { MdOutlineHelpOutline } from "react-icons/md";
 import Styles from "../../scss/components/Footer.module.scss";
 
 export const Footer: FC = () => {
   return (
     <footer className={Styles.footer}>
       <p>&copy; 2023 0bjekt/ts5h, James Cleeland</p>
-      <p>&copy; 2023 0bjekt/ts5h, James Cleeland</p>
+      <nav>
+        <MdOutlineHelpOutline className={Styles.button} onClick={() => {}} />
+      </nav>
     </footer>
   );
 };
