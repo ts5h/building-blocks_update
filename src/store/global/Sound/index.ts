9d71d6ef8cd3870b507ed60088b3d5269fcea38b
at4 <at4@jc4.so-net.ne.jp>
2024-01-10 12:42:39 +0100

    Move first touch handler to the blocks' handlers

--- src/store/global/Sound/index.ts
+++ src/store/global/Sound/index.ts
@@ -1,9 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { useFirstTouch } from "../../../hooks/useFirstTouch";
 
 export const useSound = () => {
-  const { handleFirstTouch } = useFirstTouch();
-
   const [isLoop, setIsLoop] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -13,8 +10,8 @@ export const useSound = () => {
 
   const timer = useRef<number | null>(null);
 
-  const filePath = "https://0bjekt.co/2023/building-blocks_2/sounds";
-  // const filePath = "/sounds";
+  // const filePath = "https://0bjekt.co/2023/building-blocks_2/sounds";
+  const filePath = "/sounds";
 
   // NOTE: Suspend audio correctly when isPlaying is false
   const checkAndCStopAudio = useCallback(() => {
@@ -78,9 +75,6 @@ export const useSound = () => {
 
   const startPlaying = useCallback(
     (soundFile: Sound) => {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      (async () => handleFirstTouch())();
-
       // console.log(soundFile);
       const { fileName, loop } = soundFile;
       setIsLoop(loop);
@@ -94,7 +88,7 @@ export const useSound = () => {
 
       setIsPlaying(true);
     },
-    [audioContext, handleFirstTouch, initAudio, isLoaded],
+    [audioContext, initAudio, isLoaded],
   );
 
   const stopPlaying = useCallback(() => {
